090647cecdf7ec97a02ad33a68301fe467659743
rodrigocm1995 <87797660+rodrigocm1995@users.noreply.github.com>
2025-02-19 21:25:59 -0600

    Update example1.c

--- C/ADS1115/Examples/example1.c
+++ C/ADS1115/Examples/example1.c
@@ -1,146 +1,336 @@
+/* USER CODE BEGIN Header */
 /**
-    *******************************************************************************************
-  * @file           : ADS1115.h
-  * @brief          : ADS1115 Library
-    *******************************************************************************************
-
-  * The ADS111x are precision, low-power 16-bit, I2C compatible, analog-to-digital converters
-  * (ADCs) offered in a leadless X2QFN-10 or SOT-10 package. The ADS111x devices incorporate
-  * a low drift voltage reference and an oscillator. The ADS1114 and ADS1115 also incorporate
-  * a programmable fain amplifier (PGA) and a digital comparator.
-  * 
-  * The ADS111x devices perform conversions at data rates of up to 860 samples per second (SPS)
-  * The PGA offers inpur ranges from ±256mV to ±6.144V, allowing precise large ans small-signal
-  * measurements. The ADS1115 features an input multiplexer (MUX) that allows two differential
-  * or four single-ended input measurements. Use the digital comparator in the ADS1114 and 
-  * ADS1115 for undervoltage and overvoltage protection.
-  *   
-  * @details
-  * Ultra small packages:
-  * - X2QFN: 2mm × 1.5mm × 0.4mm
-  * - SOT: 2.9mm × 2.8mm × 0.6mm
-  * Wide supply range: 2.0V to 5.5V
-  * Low current consumption: 150 µA (continuous conversion mode)
-  * Programmable data rate: 8 SPS to 860 SPS
-  * Single-cycle settling
-  * Internal low-drift voltage reference
-  * Internal oscillator
-  * I2C interface: four pin-selectable addresses
-  * Operating temperature range:
-  * -40 °C to +125°C
-  * Family of devices:
-  * - ADS1113: one single-ended (SE) or differential (DE) input
-  * - ADS1114: one single-ended (SE) or differential (DE) input with comparator and PGA
-  * - ADS1115: four single-ended or two differential inputs with comparator and PGA
-  * 
-  * @example
-  * 
-  *******************************************************************************************
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
   */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
 
-#ifndef INC_ADS1115_H_
-#define INC_ADS1115_H_
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "math.h"
+#include "ADS1115.h"
+/* USER CODE END Includes */
 
-#define ADS1115_DEFAULT_ADDRESS                  0x48
-#define ADS1115_TRIALS                          5
-#define CHECK_BIT(var,pos) ((var) & (1<<(pos)))
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
 
-// Registers
-#define ADS1115_CONVERSION_REGISTER              0x00
-#define ADS1115_CONFIGURATION_REGISTER           0x01
-#define ADS1115_THIGH_LIMIT_REGISTER             0x02
-#define ADS1115_TLOW_LIMIT_REGISTER              0x03
+/* USER CODE END PTD */
 
-typedef enum
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+I2C_HandleTypeDef hi2c1;
+
+UART_HandleTypeDef huart2;
+
+/* USER CODE BEGIN PV */
+ADS1115_t ads1115;
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART2_UART_Init(void);
+static void MX_I2C1_Init(void);
+/* USER CODE BEGIN PFP */
+static void printValue(uint16_t registerValue, char mystring[]);
+void printValueD(double registerValue, char mystring[]);
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
 {
-  AINP_IS_AIN0_AINN_IS_AIN1                    = 0x0000,
-  AINP_IS_AIN0_AINN_IS_AIN3                    = 0x1000,
-  AINP_IS_AIN1_AINN_IS_AIN3                    = 0x2000,
-  AINP_IS_AIN2_AINN_IS_AIN3                    = 0x3000,
-  AINP_IS_AIN0_AINN_IS_GND                     = 0x4000,
-  AINP_IS_AIN1_AINN_IS_GND                     = 0x5000,
-  AINP_IS_AIN2_AINN_IS_GND                     = 0x6000,
-  AINP_IS_AIN3_AINN_IS_GND                     = 0x7000,
-}ADS115Mux;
 
-typedef enum
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART2_UART_Init();
+  MX_I2C1_Init();
+  /* USER CODE BEGIN 2 */
+  //Ads1115Init(&ads1115, &hi2c1, ADS1115_DEFAULT_ADDRESS);
+  Ads1115CustomInit(&ads1115, &hi2c1, ADS1115_DEFAULT_ADDRESS, AINP_IS_AIN0_AINN_IS_AIN1, FSR_4_DOT_096_V, ADS1115_CONTINUOUS_MODE, ADS1115_860_SPS, ADS1115_TRADITIONAL_COMP, ADS1115_ACTIVE_LOW, ADS1115_NONLATCHING_COMP, ADS1115_TWO_CONVERSIONS);
+
+  uint16_t configReg = Ads1115GetConfigRegister(&ads1115);
+  printValue(configReg, "Config Value 0x%04X\r\n");
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+	 if (Ads1115IsDeviceReady(&ads1115))
+	 {
+		 double voltage = Ads1115GetVoltage(&ads1115);
+		 printValueD(voltage, "Voltage = %.5f \r\n");
+	 }
+
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
 {
-  FSR_6_DOT_144_V                             = 0x0000,
-  FSR_4_DOT_096_V                             = 0x0200,
-  FSR_2_DOT_048_V                             = 0x0400,
-  FSR_1_DOT_024_V                             = 0x0600,
-  FSR_0_DOT_512_V                             = 0x0800,
-  FSR_0_DOT_256_V                             = 0x0A00,
-}ADS115Pga;
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
-typedef enum
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+  RCC_OscInitStruct.PLL.PREDIV = RCC_PREDIV_DIV1;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART2|RCC_PERIPHCLK_I2C1;
+  PeriphClkInit.Usart2ClockSelection = RCC_USART2CLKSOURCE_PCLK1;
+  PeriphClkInit.I2c1ClockSelection = RCC_I2C1CLKSOURCE_HSI;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void)
 {
-  ADS1115_CONTINUOUS_MODE                     = 0x0000,
-  ADS1115_SINGLE_SHOT                         = 0x0100,  
-} ADS115Mode;
 
-typedef enum
+  /* USER CODE BEGIN I2C1_Init 0 */
+
+  /* USER CODE END I2C1_Init 0 */
+
+  /* USER CODE BEGIN I2C1_Init 1 */
+
+  /* USER CODE END I2C1_Init 1 */
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.Timing = 0x00201D2B;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Analogue filter
+  */
+  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c1, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Digital filter
+  */
+  if (HAL_I2CEx_ConfigDigitalFilter(&hi2c1, 0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C1_Init 2 */
+
+  /* USER CODE END I2C1_Init 2 */
+
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
 {
-  ADS1115_8_SPS                               = 0x0000,
-  ADS1115_16_SPS                              = 0x0020,
-  ADS1115_32_SPS                              = 0x0040,
-  ADS1115_64_SPS                              = 0x0060,
-  ADS1115_128_SPS                             = 0x0080,
-  ADS1115_250_SPS                             = 0x00A0,
-  ADS1115_475_SPS                             = 0x00C0,
-  ADS1115_860_SPS                             = 0x00E0,
-} ADS1115DataRate;
 
-typedef enum
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart2.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart2.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
 {
-  ADS1115_TRADITIONAL_COMP                    = 0x0000,
-  ADS1115_WINDOW_COMP                         = 0x0010,
-} ADS1115CompMode;
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
 
-typedef enum
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOF_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : B1_Pin */
+  GPIO_InitStruct.Pin = B1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : LD2_Pin */
+  GPIO_InitStruct.Pin = LD2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+void printValue(uint16_t registerValue, char mystring[]){
+  char outputData[50] = {'\0'};
+  uint8_t outputDataSize = 0;
+  outputDataSize = sprintf(outputData, mystring, registerValue );
+  HAL_UART_Transmit(&huart2, outputData, outputDataSize, HAL_MAX_DELAY);
+}
+
+void printValueD(double registerValue, char mystring[]){
+  char outputData[50] = {'\0'};
+  uint8_t outputDataSize = 0;
+  outputDataSize = sprintf(outputData, mystring, registerValue );
+  HAL_UART_Transmit(&huart2, outputData, outputDataSize, HAL_MAX_DELAY);
+}
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
 {
-  ADS1115_ACTIVE_LOW                          = 0x0000,
-  ADS1115_ACTIVE_HIGH                         = 0x0008,
-} ADS1115CompPol;
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
 
-typedef enum
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
 {
-  ADS1115_NONLATCHING_COMP                    = 0x0000,
-  ADS1115_LATCHING_COMP                       = 0x0004,
-} ADS1115CompLatch;
-
-typedef enum
-{
-  ADS1115_ONE_CONVERSION                      = 0x0000,
-  ADS1115_TWO_CONVERSIONS                     = 0x0001,
-  ADS1115_FOUR_CONVERSIONS                    = 0x0002,
-  ADS1115_DISABLE_COMPARATOR                  = 0x0003,
-} ADS1115CompQueue;
-
-typedef struct
-{
-  I2C_HandleTypeDef *hi2c;
-  uint8_t devAddress;
-  double lsbSize;
-}ADS1115_t;
-
-
-void Ads1115WriteRegister(ADS1115_t *ads1115, uint8_t registerAddress, uint16_t value);
-
-uint16_t Ads1115ReadRegister(ADS1115_t *ads1115, uint8_t registerAddress);
-
-uint8_t Ads1115Init(ADS1115_t *ads1115, I2C_HandleTypeDef *i2c, uint8_t devAddress);
-
-uint8_t Ads1115CustomInit(ADS1115_t *ads1115, I2C_HandleTypeDef *i2c, uint8_t devAddress, ADS115Mux muxType, ADS115Pga pga, ADS115Mode mode, ADS1115DataRate dataRate, ADS1115CompMode compMode, ADS1115CompPol pol, ADS1115CompLatch latch, ADS1115CompQueue queue);
-
-uint16_t Ads1115GetConfigRegister(ADS1115_t *ads1115);
-
-uint16_t Ads1115GetConversion(ADS1115_t *ads1115);
-
-double Ads1115GetVoltage(ADS1115_t *ads1115);
-
-_Bool Ads1115IsDeviceReady(ADS1115_t *ads1115);
-
-
-#endif
-
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
