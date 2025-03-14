aade8016e42fd23dac0ad2a4682c4cf17fc8b540
rodrigocm1995 <87797660+rodrigocm1995@users.noreply.github.com>
2025-03-14 09:58:02 -0600

    Update BQ32002.h

--- C/BQ32002/BQ32002.h
+++ C/BQ32002/BQ32002.h
@@ -1,1 +1,6 @@
+#ifndef INC_BQ32002_H_
+#define INC_BQ32002_H_
 
+#define BQ32002_DEFAULT_ADDRESS                  0x48
+#define BQ32002_TRIALS                          5
+#define CHECK_BIT(var,pos) ((var) & (1<<(pos)))
