23308d5420a7229f900881191f20ac1e44ca0d76
rodrigocm1995 <87797660+rodrigocm1995@users.noreply.github.com>
2025-05-16 21:19:09 -0600

    Update SerialPrint.h

--- C/SerialPrint/SerialPrint.h
+++ C/SerialPrint/SerialPrint.h
@@ -1,1 +1,20 @@
+/**
+    *******************************************************************************************
+  * @file           : SerialPrint.h
+  * @brief          : SerialPrint
+    *******************************************************************************************
+  */
+
+#ifndef INC_SERIALPRINT_H_
+#define INC_SERIALPRINT_H_
+
+typedef struct
+{
+  UART_HandleTypeDef *huart;
+}SerialPrint_t;
+
+void serialPrint_init(SerialPrint_t *serialPrint, UART_HandleTypeDef *huart);
+void serialPrint(SerialPrint_t *serialPrint, uint32_t value, char customString[]);
+
+#endif
 
