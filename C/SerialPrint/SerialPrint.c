f7406e7821e3ed71e5facf9ad8f0c7175f0330ae
rodrigocm1995 <87797660+rodrigocm1995@users.noreply.github.com>
2025-05-16 21:19:37 -0600

    Update SerialPrint.c

--- C/SerialPrint/SerialPrint.c
+++ C/SerialPrint/SerialPrint.c
@@ -1,1 +1,16 @@
+#include "main.h"
+#include "SerialPrint.h"
+
+void serialPrint_init(SerialPrint_t *serialPrint, UART_HandleTypeDef *huart){
+	serialPrint->huart = huart;
+}
+
+serialPrint(SerialPrint_t *serialPrint, uint32_t value, char customString[]){
+
+  uint8_t outputDataSize = 0;
+  char outputData[50] = {'\0'};
+
+  outputDataSize = sprintf(outputData, customString, value);
+  HAL_UART_Transmit(serialPrint->huart, outputData, outputDataSize, HAL_MAX_DELAY);
+}
 
