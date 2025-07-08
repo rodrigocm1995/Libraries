c62c5cd081c21d479c04869682823631fbb73db5
rodrigocm1995 <87797660+rodrigocm1995@users.noreply.github.com>
2025-07-08 17:18:15 -0600

    Update INA229.h

--- C/INA229/INA229.h
+++ C/INA229/INA229.h
@@ -61,7 +61,7 @@
 
 #define CONFIG              0x00U
 #define ADC_CONFIG          0x01U
-#define SHUNT_CALL          0x02U
+#define SHUNT_CAL           0x02U
 #define SHUNT_TEMPCO        0x03U
 #define VSHUNT              0x04U
 #define VBUS                0x05U
@@ -80,15 +80,121 @@
 #define MANUFACTURER_ID     0x3EU
 #define DEVICE_ID           0x3FU
 
+#define INA229_SCALING_FACTOR 				13107.2E6
+#define INA229_TEMP_CONV_FACTOR 			7.8125E-3
+#define INA229_VSHUNT_CONV_FACTOR_ADC_0		312.5E-9
+#define INA229_VSHUNT_CONV_FACTOR_ADC_1		78.125E-9
 
+#define CHECK_BIT(var,pos) ((var) &  (1 << (pos)))
+#define _SET_BIT(var,pos)  ((var) |= (1 << (pos)))
+#define _CLEAR_BIT(var,pos)  ((var) &= ~(1 << (pos)))
 
-#define CHECK_BIT(var,pos) ((var) & (1<<(pos)))
+//********************************************************************************************
+//CONFIG REGISTER
+typedef enum {
+	INA229_0_S				= 0x0000,
+	INA229_2_MS				= 0x0040,
+	INA229510_MS			= 0x3FC0,
+}Ina229_Conv_Dly;
 
 typedef enum {
-  INA229_ADC_163DOT84_MILIVOLT    = 0x0000, // ADCRANGE = 0 (±163.84mV)
-  INA229_ADC_40DOT96_MILIVOLT     = 0x0010  // ADCRANGE = 1 (±40.96mV). 625nV/LSB
-} INA229_AdcRange_t;
+	INA229_TEMP_COMP_DIS	= 0x0000,
+	INA229_TEMP_COMP_ENA	= 0x0020,
+}Ina229_Temp_Comp;
 
+typedef enum {
+  INA229_ADC_163DOT84_MILIVOLT    = 0x0000, // ADCRANGE = 0 (±163.84mV) = 312.5 nV/LSB
+  INA229_ADC_40DOT96_MILIVOLT     = 0x0010  // ADCRANGE = 1 (±40.96mV)  = 78.125 nV/LSB
+}Ina229_Adc_Range;
+//END OF CONFIG REGISTER
+//********************************************************************************************
+
+
+//********************************************************************************************
+//ADC CONFIG REGISTER
+typedef enum {
+	INA229_SHUNTDOWN				= 0x0000,
+	INA229_TRIG_BUS					= 0x1000,
+	INA229_TRIG_SHUNT				= 0x2000,
+	INA229_TRIG_BUS_SHUNT			= 0x3000,
+	INA229_TRIG_TEMP				= 0x4000,
+	INA229_TRIG_TEMP_BUS			= 0x5000,
+	INA229_TRIG_TEMP_SHUNT			= 0x6000,
+	INA229_TRIG_BUS_SHUNT_TEMP		= 0x7000,
+	INA229_BUS_CONT					= 0x9000,
+	INA229_SHUNT_CONT				= 0xA000,
+	INA229_BUS_SHUNT_CONT			= 0xB000,
+	INA229_TEMP_CONT				= 0xC000,
+	INA229_BUS_TEMP					= 0xD000,
+	INA229_TEMP_SHUNT				= 0xE000,
+	INA229_BUS_SHUNT_TEMP_CONT		= 0xF000,
+}Ina229_Mode;
+
+typedef enum {
+	INA229_VBUS_50_US				= 0x0000,
+	INA229_VBUS_84_US				= 0x0200,
+	INA229_VBUS_150_US				= 0x0400,
+	INA229_VBUS_280_US				= 0x0600,
+	INA229_VBUS_540_US				= 0x0800,
+	INA229_VBUS_1052_US				= 0x0A00,
+	INA229_VBUS_2074_US				= 0x0C00,
+	INA229_VBUS_4120_US				= 0x0E00,
+}Ina229_VBus_CT;
+
+typedef enum {
+	INA229_SHUNT_50_US				= 0x0000,
+	INA229_SHUNT_84_US				= 0x0040,
+	INA229_SHUNT_150_US				= 0x0080,
+	INA229_SHUNT_280_US				= 0x00C0,
+	INA229_SHUNT_540_US				= 0x0100,
+	INA229_SHUNT_1052_US			= 0x0140,
+	INA229_SHUNT_2074_US			= 0x0180,
+	INA229_SHUNT_4120_US			= 0x01C0,
+}Ina229_Shunt_CT;
+
+typedef enum {
+	INA229_TEMP_50_US				= 0x0000,
+	INA229_TEMP_84_US				= 0x0008,
+	INA229_TEMP_150_US				= 0x0010,
+	INA229_TEMP_280_US				= 0x0018,
+	INA229_TEMP_540_US				= 0x0020,
+	INA229_TEMP_1052_US				= 0x0028,
+	INA229_TEMP_2074_US				= 0x0030,
+	INA229_TEMP_4120_US				= 0x0038,
+}Ina229_Temp_CT;
+
+typedef enum {
+	INA229_1_SAMPLE					= 0x0000,
+	INA229_4_SAMPLES				= 0x0001,
+	INA229_16_SAMPLES				= 0x0002,
+	INA229_64_SAMPLES				= 0x0003,
+	INA229_128_SAMPLES				= 0x0004,
+	INA229_256_SAMPLES				= 0x0005,
+	INA229_512_SAMPLES				= 0x0006,
+	INA229_1024_SAMPLES				= 0x0007
+}Ina229_Avg;
+//END OF ADC CONFIG REGISTER
+//********************************************************************************************
+
+typedef enum {
+	INA229_TRANSPARENT				= 0x0000,
+	INA229_LATCHED					= 0x8000,
+}INA229_Alert_Latch;
+
+typedef enum {
+	INA229_DIS_CNVR_ALERT_PIN		= 0x0000,
+	INA229_EN_CNVR_ALERT_PIN		= 0x4000,
+}Ina229_CNVR;
+
+typedef enum {
+	INA229_DIS_COMPARISON_ALERT		= 0x0000,
+	INA229_EN_COMPARISON_ALERT		= 0x2000,
+}INA229_Slow_Alert;
+
+typedef enum {
+	INA229_ACTIVE_LOW_ALERT			= 0x0000,
+	INA229_ACTIVE_HIGH_ALERT		= 0x1000,
+}INA229_Alert_Polarity;
 
 
 typedef struct
@@ -109,16 +215,66 @@ typedef struct
   uint16_t			tempLimitRegister;
   uint16_t			pwrLimitRegister;
 
-  uint16_t			adcRange;
+  _Bool				adcRange;
   double			currentLsb;
+  double 			shuntResistor;
+  double 			maximumCurrent;
+  double 			shuntCal;
+  double 			vshuntConvFactor;
 }INA229_t;
 
 
-void INA229_Init(INA229_t *ina229, SPI_HandleTypeDef *spiHandle, GPIO_TypeDef *csPort, uint16_t csPin);
+void INA229_Init(INA229_t *ina229,
+		SPI_HandleTypeDef *spiHandle,
+		GPIO_TypeDef *csPort,
+		uint16_t csPin);
 
-uint64_t INA229_ReadRegister(INA229_t *ina229, uint8_t registerAddress);
+void INA229_Custom_Init(INA229_t *ina229,
+		SPI_HandleTypeDef *spiHandle,
+		GPIO_TypeDef *csPort,
+		uint16_t csPin,
+		Ina229_Conv_Dly dly,
+		Ina229_Temp_Comp tpComp,
+		Ina229_Adc_Range adcRange);
 
-uint8_t INA229_WriteRegister(INA229_t *ina229, uint8_t registerAddress, uint8_t value);
+void INA229_ADC_Config(INA229_t *ina229,
+		Ina229_Mode mode,
+		Ina229_VBus_CT cbusCt,
+		Ina229_Shunt_CT shuntCt,
+		Ina229_Temp_CT tempCt,
+		Ina229_Avg avg);
+
+uint64_t INA229_ReadRegister(INA229_t *ina229,
+		uint8_t registerAddress);
+
+uint8_t INA229_WriteRegister(INA229_t *ina229,
+		uint8_t registerAddress,
+		uint16_t value);
+
+void INA229_Reset_Registers(INA229_t *ina229);
+
+void INA229_SetCalibration(INA229_t *ina229,
+		double shuntResistor,
+		uint16_t maxCurrent);
+
+double INA229_Get_Current(INA229_t *ina229);
+
+double INA229_Get_Shunt_Voltage(INA229_t *ina229);
+
+double INA229_Get_Power(INA229_t *ina229);
+
+double INA229_Get_Energy(INA229_t *ina229);
+
+double INA229_Get_Charge(INA229_t *ina229);
+
+double INA229_Get_Temperature(INA229_t *ina229);
+
+void INA229_Set_Alert(INA229_t *ina229);
+
+_Bool INA229_Get_Energy_OverFlow(INA229_t *ina229);
+
+_Bool INA229_Get_Charge_OverFlow(INA229_t *ina229);
+
+_Bool INA229_Data_Ready(INA229_t *ina229);
 
 #endif
-
